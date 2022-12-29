5010e07c967e2d62d4c107955460c175a34aff5e
Tim New Gucci <67848425+timtimmy9@users.noreply.github.com>
2022-12-29 12:02:54 +0900

    Update form.php
    Added a name for the submit button

--- form.php
+++ form.php
@@ -5,8 +5,7 @@
 <form action="temp.php" method="post">
 Name: <input type="text" name="name"><br>
 Temperature: <input type="text" name="temperature"><br>
-<input type="submit">
-</form>
+<input type="submit" name="submit">
 
 </body>
 </html>
